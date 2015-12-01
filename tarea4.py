31552028770b0d94cfb66e681a8461370df56107
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2015-12-01 20:58:42 -0300

    Add plotting and stuff

--- tarea4.py
+++ tarea4.py
@@ -12,13 +12,73 @@ import time
 from multilayer_perceptron.multilayer_perceptron import MultilayerPerceptronClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+def plot_confusion_matrix(cm, title='Confusion matrix', no_code=True, cmap=plt.cm.Blues, ticks=None):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    if ticks:
+        plt.yticks(np.arange(0,7), ticks, fontsize=12)
+        plt.xticks(np.arange(0,7), ticks, rotation=70, fontsize=9)
+    width = len(cm)
+    height = len(cm[0])
+    for x in xrange(width):
+        for y in xrange(height):
+            plt.annotate(str(cm[x][y])[:4], xy=(y, x), horizontalalignment='center', verticalalignment='center', fontsize=10)
+
+
+
 def read_mat(file_url):
     if not os.path.exists(file_url):
         print "Archivo", file_url, "no existe"
         return None
     return scipy.io.loadmat(file_url)
 
-def generate_matrix(categories, set_type='Train'):
+def category_code(category, no_code = True):
+    if no_code:
+        return category
+    codes = {'Auditorium': [0,0,0],
+            'bar': [0,0,1],
+            'classroom': [0,1,0],
+            'closet': [0,1,1],
+            'movietheater': [1,0,0],
+            'restaurant': [1,0,1],}
+    return codes[category];
+
+def category_decode(code):
+    if (code == np.array([0,0,0])).all():
+        return 'Auditorium'
+    elif (code == np.array([0,0,1])).all():
+        return 'bar'
+    elif (code == np.array([0,1,0])).all():
+        return 'classroom'
+    elif (code == np.array([0,1,1])).all():
+        return 'closet'
+    elif (code == np.array([1,0,0])).all():
+        return 'movietheater'
+    elif (code == np.array([1,0,1])).all():
+        return 'restaurant'
+    else:
+        return 'other'
+
+def matrix_category_decode(matrix):
+    new_matrix = []
+    for item in matrix:
+        new_matrix.append(category_decode(item))
+    return np.array(new_matrix)
+
+def normalize_matrix(matrix):
+    new_matrix = []
+    for row in matrix:
+        row_sum = sum(row)
+        if row_sum != 0:
+            new_row = [float(item)/row_sum for item in row]
+            new_matrix.append(new_row)
+        else:
+            new_matrix.append(row)
+    return new_matrix
+
+def generate_matrix(categories, set_type='Train', no_code=False):
     all_data = []
     all_categories = []
     folder_prefix = 'FC6'
@@ -28,7 +88,7 @@ def generate_matrix(categories, set_type='Train'):
         for mat_file in glob.glob(os.path.join(folder_prefix+set_type, category+set_type)+'/*.mat'):
             mat_row = read_mat(mat_file)['stored'][0]
             all_data.append(mat_row)
-            all_categories.append(category)
+            all_categories.append(category_code(category, no_code))
 
     all_data_array = np.array(all_data)
     all_categories_array = np.array(all_categories)
@@ -37,41 +97,47 @@ def generate_matrix(categories, set_type='Train'):
 
 if __name__ == '__main__':
     categories = ['Auditorium', 'bar', 'classroom', 'closet', 'movietheater',
-        'restaurant']
+        'restaurant', 'other']
 
-    data_train, category_train = generate_matrix(categories, 'Train')
-    data_test, category_test = generate_matrix(categories, 'Test')
+    no_code = False
+
+    data_train, category_train = generate_matrix(categories, 'Train', no_code)
+    data_test, category_test = generate_matrix(categories, 'Test', no_code = True)
 
     perceptron = MultilayerPerceptronClassifier( \
-          hidden_layer_sizes = (500,), \
-          max_iter = 6000, \
+          hidden_layer_sizes = (100,), \
+          max_iter = 1000, \
         #   random_state = 123,
           algorithm = 'sgd',
           batch_size = 200,
           shuffle = True,
           alpha = 0.00002,\
-          learning_rate_init = 0.0001, \
-          verbose = True,)
+          learning_rate_init = 0.0001, )
+        #   verbose = True,)
 
     category_predicted = perceptron.fit(data_train, category_train).predict(data_test)
+    if not no_code:
+        category_predicted = matrix_category_decode(category_predicted)
+
 
     accuracy = accuracy_score(category_test, category_predicted)
+    cm = confusion_matrix(category_test, category_predicted, categories)
 
-    cm = confusion_matrix(category_test, category_predicted)
     # cm_title = 'Confusion matrix, without normalization ('+selected_set_name+', '+perceptr_name+')'
     # print(cm_title)
     # print(cm)
     # plt.figure()
     # plot_confusion_matrix(cm, cm_title, ticks=tuple(actions))
 
-    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    # cm_normalized_title = 'Normalized confusion matrix ('+selected_set_name+', '+perceptr_name+') '+accuracy
-    # print cm_normalized_title
-    # print(cm_normalized)
-    # plt.figure()
-    # plot_confusion_matrix(cm_normalized, cm_normalized_title, ticks=tuple(actions))
+    cm_normalized = normalize_matrix(cm)
+
+    coded_title = 'String coded' if no_code else 'List coded'
+    outputs = perceptron.n_outputs_
+    cm_normalized_title = coded_title + ' outputs: ' +str(outputs) +' acc: '+str(accuracy)
+    plt.figure()
+    plot_confusion_matrix(cm_normalized, cm_normalized_title, no_code, ticks=(categories))
 
     print 'Accuracy: ' + str(accuracy)
 
-    # plt.savefig('cm.png', bbox_inches='tight')
-    # plt.close()
+    plt.savefig('cm_'+coded_title.lower().replace(" ", "_")+'_'+str(time.time()).replace('.','')+'.png', bbox_inches='tight')
+    plt.close()
